6f3f4eb9887a548f2288e5d69152615873762925
Dione Neris <dioneneris@gmail.com>
2022-08-16 22:27:30 -0300

    Projeto inicial

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function start() {
+  var buttonCalculatePreco = document.querySelector(button - calcular);
+}
+
+function precoFinal(preco, margem) {
+  return preco + (margem / 100) * preco;
+}
+start();
